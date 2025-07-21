b26d759a60cff8a340a036cae1952a623cfff20d
Om Mahadik <ommahadik245@gmail.com>
2025-07-21 09:40:56 +0530

    Bitwise Operator Example

---
+++ usingBitwiseOp.c
@@ -0,0 +1,15 @@
+// Write ac program to check if the number is even or odd using bitwise operator without using stdbool.h
+
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter an integer value:\\n");
+    scanf("%d", &num);
+
+    if (num & 1) {
+        printf("The number %d is odd.\n", num);
+    } else {
+        printf("The number %d is even.\n", num);
+        
+    }
+}
